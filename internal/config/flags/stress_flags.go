b5a224c491560ac91379a842e1f6150a9a5f1941
kropidlowsky <kropidlowsky@protonmail.com>
2025-01-12 21:49:27 +0100

    add parsing stress flags

--- internal/config/flags/stress_flags.go
+++ internal/config/flags/stress_flags.go
@@ -6,7 +6,7 @@ import (
 	"github.com/kropidlowsky/pln-checker/internal/config/flags/options"
 )
 
-func ParseStressFlags() {
+func ParseStressFlags(args []string) (*options.StressOpts, error) {
 	fs := flag.NewFlagSet("stress tests", flag.ExitOnError)
 
 	opts := &options.StressOpts{}
@@ -14,4 +14,10 @@ func ParseStressFlags() {
 	fs.Var(&opts.Host, "Host", "Host to send the request to")
 	fs.UintVar(&opts.Rate, "X", 0, "Numer of requests per the frequency (Y)")
 	fs.UintVar(&opts.Frequency, "Y", 0, "Interval for X requests")
+
+	if err := fs.Parse(args); err != nil {
+		return nil, err
+	}
+
+	return opts, nil
 }
