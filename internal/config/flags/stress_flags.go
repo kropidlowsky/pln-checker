67d15a2666f069e4a835fac86af05706bd2d1305
kropidlowsky <kropidlowsky@protonmail.com>
2025-01-12 23:10:10 +0100

    make Y usage more clear

--- internal/config/flags/stress_flags.go
+++ internal/config/flags/stress_flags.go
@@ -13,7 +13,7 @@ func ParseStressFlags(args []string) (*options.StressOpts, error) {
 
 	fs.Var(&opts.Host, "Host", "Host to send the request to")
 	fs.UintVar(&opts.Rate, "X", 0, "Numer of requests per the frequency (Y)")
-	fs.UintVar(&opts.Frequency, "Y", 0, "Interval for X requests")
+	fs.UintVar(&opts.Frequency, "Y", 0, "Interval in seconds for making X requests at once")
 
 	if err := fs.Parse(args); err != nil {
 		return nil, err
