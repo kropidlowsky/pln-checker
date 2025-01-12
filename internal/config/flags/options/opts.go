1f3c2bc149660a874d7f0be57f73a42a63818cb8
kropidlowsky <kropidlowsky@protonmail.com>
2025-01-12 20:39:14 +0100

    add opts Set

--- internal/config/flags/options/opts.go
+++ internal/config/flags/options/opts.go
@@ -1,6 +1,19 @@
 package options
 
+import "net/url"
+
 // Opts represents common options.
 type Opts struct {
-	URL string
+	Host *url.URL
+}
+
+func (o *Opts) Set(host string) error {
+	u, err := url.Parse(host)
+	if err != nil {
+		return err
+	}
+
+	o.Host = u
+
+	return nil
 }
