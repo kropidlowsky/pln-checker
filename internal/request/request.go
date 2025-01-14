f0df7ca418c291eb4bd1e98d0e74065a3a9fa66b
kropidlowsky <kropidlowsky@protonmail.com>
2025-01-14 22:22:57 +0100

    add request method

--- internal/request/request.go
+++ internal/request/request.go
@@ -6,26 +6,47 @@ import (
 )
 
 type Request struct {
-	host string
+	client *http.Client
+	host   string
 }
 
 func NewRequest(host string) Request {
 	return Request{
-		host: host,
+		client: http.DefaultClient,
+		host:   host,
 	}
 }
 
 func (r Request) Get() (RequestResult, error) {
 	start := time.Now()
 
-	resp, err := http.Get(r.host)
+	req, err := r.request()
 	if err != nil {
 		return RequestResult{}, err
 	}
+
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return RequestResult{}, err
+	}
+
+	defer resp.Body.Close()
 
 	duration := time.Since(start)
 
 	result := NewRequestResult(*resp, start, duration)
 
 	return result, nil
+}
+
+// request creates the request which includes User-Agent header required by some APIs.
+func (r Request) request() (*http.Request, error) {
+	req, err := http.NewRequest(http.MethodGet, r.host, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "")
+
+	return req, nil
 }
