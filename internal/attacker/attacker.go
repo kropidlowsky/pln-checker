7cb16e0def80007b5a844ae089d1d138a5faea9b
kropidlowsky <kropidlowsky@protonmail.com>
2025-01-14 22:44:00 +0100

    InfiniteAttack

--- internal/attacker/attacker.go
+++ internal/attacker/attacker.go
@@ -2,6 +2,11 @@ package attacker
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/kropidlowsky/pln-checker/internal/config/options"
 	"github.com/kropidlowsky/pln-checker/internal/request"
@@ -13,7 +18,8 @@ type Attacker struct {
 	frequency uint
 	results   []request.RequestResult
 
-	// rw sync.RWMutex
+	wg sync.WaitGroup
+	rw sync.RWMutex
 }
 
 func NewAttacker(opts options.LoadOpts) *Attacker {
@@ -24,21 +30,43 @@ func NewAttacker(opts options.LoadOpts) *Attacker {
 	}
 }
 
-// Attack performs
+func (a *Attacker) InfiniteAttack() {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for t := range ticker.C {
+			fmt.Println(t)
+			a.Attack()
+		}
+	}()
+
+	<-sigChan
+}
+
+// Attack performs many attack at the same time.
 func (a *Attacker) Attack() {
 	for i := 0; i < int(a.rate); i++ {
-		a.attack()
+		a.wg.Add(1)
+		a.singleAttack()
 	}
 }
 
-// attack performs one attack.
-func (a *Attacker) attack() {
+// singleAttack performs one attack.
+func (a *Attacker) singleAttack() {
+	defer a.wg.Done()
+
 	req := request.NewRequest(a.host)
+
 	result, err := req.Get()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v", result)
+	a.rw.Lock()
+	defer a.rw.Unlock()
 	a.results = append(a.results, result)
 }
