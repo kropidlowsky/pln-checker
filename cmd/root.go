394ab9cca3fdd4588a4f1c2963a80829d9e5cf54
kropidlowsky <kropidlowsky@protonmail.com>
2025-01-14 17:47:26 +0100

    init root cmd

---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/kropidlowsky/pln-checker/internal/config/options"
+	"github.com/spf13/cobra"
+)
+
+var opts = &options.StressOpts{}
+
+var rootCmd = &cobra.Command{
+	Use:   "pln-checker",
+	Short: "load test",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().Var(&opts.Host, "Host", "Host to send the request to")
+	rootCmd.PersistentFlags().UintVar(&opts.Rate, "X", 0, "Numer of requests per the frequency (Y)")
+	rootCmd.PersistentFlags().UintVar(&opts.Frequency, "Y", 0, "Interval in seconds for making X requests at once")
+}
