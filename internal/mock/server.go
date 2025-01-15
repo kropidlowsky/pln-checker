6e69467c718b68c4e069b0a635a8ca387004a764
kropidlowsky <kropidlowsky@protonmail.com>
2025-01-15 22:47:35 +0100

    setup mock server

---
+++ internal/mock/server.go
@@ -0,0 +1,23 @@
+package mock
+
+import (
+	"net/http"
+
+	"github.com/jarcoal/httpmock"
+)
+
+func SetupMockServer() {
+	httpmock.Activate()
+
+	httpmock.RegisterResponder(http.MethodGet, "http://example.com/json",
+		httpmock.NewJsonResponderOrPanic(http.StatusOK, map[string]string{"test": "test"}))
+
+	httpmock.RegisterResponder(http.MethodGet, "http://example.com/bad-json",
+		httpmock.NewStringResponder(http.StatusOK, `{"test": "test"}`))
+
+	httpmock.RegisterResponder(http.MethodGet, "http://example.com/text",
+		httpmock.NewStringResponder(http.StatusOK, "test"))
+
+	httpmock.RegisterResponder(http.MethodGet, "http://example.com/empty",
+		httpmock.NewStringResponder(http.StatusOK, ""))
+}
